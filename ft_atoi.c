503f8696c9af3b6ef04c848336a0d41cc952523a
Kyle <kyle@laptop.com>
2024-10-10 15:57:54 +0200

    still studying

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ncampo <ncampo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 02:02:21 by ncampo            #+#    #+#             */
-/*   Updated: 2024/10/10 02:07:59 by ncampo           ###   ########.fr       */
+/*   Updated: 2024/10/10 15:51:07 by ncampo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,40 @@
 int	ft_atoi(const char *str)
 {
 	int	i;
-	int	s;
-	int	n;
+	int	sign;
+	int	result;
 
 	i = 0;
-	s = 1;
-	n = 0;
+	sign = 1;
+	result = 0;
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			s = -1;
+			sign = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		n = n * 10 + (str[i] - '0');
+		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	return (n * s);
+	return (result * sign);
 }
-
 /*
 int	main(void)
 {
-	const char	*s1 = "    -1234";
+	const char	*s1 = "    ---+--+123ab567";
 	const char	*s2 = "Mira aqui hay un 2";
-	const char	*s3 = "+++5--6-";
-	const char	*s4 = "a-62956";
+	const char	*s3 = "-456";
+	const char	*s4 = "1234";
 
 	printf("%d\n", ft_atoi(s1));
 	printf("%d\n", ft_atoi(s2));
 	printf("%d\n", ft_atoi(s3));
 	printf("%d\n", ft_atoi(s4));
 	return (0);
+
 }
 */
