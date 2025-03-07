1b7d0bfabd40c829ce7dddf3b084fcbdaeeded86
Nina Frances Kyle Campo <ncampo@c2r1s6.42madrid.com>
2025-03-07 15:31:31 +0100

    ¨d¨

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ncampo <ncampo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 23:07:49 by ncampo            #+#    #+#             */
-/*   Updated: 2025/03/05 17:26:56 by ncampo           ###   ########.fr       */
+/*   Updated: 2025/03/07 15:29:25 by ncampo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,3 +52,16 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+/*
+int	main(void)
+{
+	ft_printf("Character: %c\n", 'A');
+	ft_printf("String: %s\n", "Hello, World!");
+	ft_printf("Pointer: %p\n", (void *)0x123456);
+	ft_printf("Integer: %d\n", 42);
+	ft_printf("Unsigned: %u\n", 123);
+	ft_printf("Hexadecimal (lower): %x\n", 255);
+	ft_printf("Hexadecimal (upper): %X\n", 255);
+	ft_printf("Percent sign: %%\n");
+	return (0);
+}*/
