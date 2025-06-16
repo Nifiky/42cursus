44983f5c45ff501fad7344bc55b5cb31eee01869
Nina Frances Kyle Campo <ncampo@c1r1s1.42madrid.com>
2025-06-16 15:43:44 +0200

    mini_atoi

--- exam02/level2/ft_atoi/ft_atoi.c
+++ exam02/level2/ft_atoi/ft_atoi.c
@@ -3,30 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ncampo <ncampo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 20:22:40 by ojamal            #+#    #+#             */
-/*   Updated: 2023/03/20 20:30:05 by ojamal           ###   ########.fr       */
+/*   Updated: 2025/06/16 15:43:26 by ncampo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(const char *str)
+int mini_atoi(char *str)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-	}
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while(str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	return result * sign;
+	int i = -1;
+	int res = 0;
+	while(str[++i])
+		res = res * 10 + str[i] - '0';
+	return res;
 }
