171df9fad27c5c2063bb3e982165eebb42545430
Nifiky <168598011+Nifiky@users.noreply.github.com>
2025-06-27 00:16:10 +0200

    Update ft_atoi.c

--- exam02/level2/ft_atoi/ft_atoi.c
+++ exam02/level2/ft_atoi/ft_atoi.c
@@ -12,9 +12,21 @@
 
 int mini_atoi(char *str)
 {
-	int i = -1;
-	int res = 0;
-	while(str[++i])
-		res = res * 10 + str[i] - '0';
-	return res;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	while(str[i] == ' ' || str[i] == '\t')
+		i++;
+	if(str[i] == '-')
+	{
+		sign = -1;
+	}
+	if(str[i] == '-' || str[i] == '+')
+		i++;
+	while(str[i] >= '0' && str[i] <= '9')
+		{
+			result = result * 10 + str[i] - '0';
+			i++;
+		}
+	return result - sign;
 }
